00dc21b2590a93cb25d536b495659dbc6aabe0d5
Laura Menegazzo Boeing <l.boeing@escola.pr.gov.br>
2026-05-07 11:20:31 -0300

    fim

---
+++ arrays.js
@@ -0,0 +1,11 @@
+const listaCelular = [
+    {nome:"Samsung Galaxy S20", preco: 3500},
+    {nome:"iPhone 11", preco: 2000},
+    {nome:"Xiaomi Mi 10", preco: 1000},
+    {nome:"OnePlus 8 Pro", preco: 1900},
+    {nome:"Google Pixel 4", preco: 800},
+    {nome:"Huawei P40 Pro", preco: 3000},
+    {nome:"Sony Xperia 1 II", preco: 2500}
+];
+
+module.exports = listaCelular;
